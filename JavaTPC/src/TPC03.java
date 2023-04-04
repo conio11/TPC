0ca9e566af994846078fb9484339664706663c08
conio11 <conio416@naver.com>
2023-04-04 15:00:34 +0900

    수정 완료

--- JavaTPC/src/TPC03.java
+++ JavaTPC/src/TPC03.java
@@ -1,12 +1,39 @@
+import kr.bit.Book;
+import kr.bit.PersonVO;
+
 
 public class TPC03 {
 	public static void main(String[] args) {
 		// 관계를 이해하라 
 		// primitive VS user defined
+		
 		// 정수 1개를 저장하기 위한 변수를 선언하시오
 		int a;
 		a = 10;
 		
 		// 책 1권을 저장하기 위한 변수를 선언하시오
+		Book b;
+		b = new Book(); // 생성자로 객체 생성 // b: 인스턴스 변수, 객체 변수
+		b.title = "자바";
+		b.price = 15000;
+		b.company = "한빛미디어";
+		b.page = 700;
+		
+		System.out.print(b.title + "\t");
+		System.out.print(b.price + "\t");
+		System.out.print(b.company + "\t");
+		System.out.println(b.page);
+		
+		PersonVO p;
+		p = new PersonVO();
+		p.name = "박매일";
+		p.age = 40;
+		p.weight = 68.4f;
+		p.height = 175.7f;
+		
+		System.out.print(p.name + "\t");
+		System.out.print(p.age + "\t");
+		System.out.print(p.weight + "\t");
+		System.out.println(p.height + "\t");
 	}
 }
